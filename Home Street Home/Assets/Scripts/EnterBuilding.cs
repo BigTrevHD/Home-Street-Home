c1aa82f145d0ecf98d326dd57e339fbe996ffda7
BigTrevHD <34038408+BigTrevHD@users.noreply.github.com>
2019-03-07 15:12:14 +0000

    Scene Mangement & UI Scaling
    - Added scene management to allow building entrance
    - Fixed UI Scaling Issues

---
+++ Home Street Home/Assets/Scripts/EnterBuilding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterBuilding : MonoBehaviour {
+    public int goToScene;
+    public int currentScene;
+
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other)
+    {
+        if(other.CompareTag("Player"))
+        SceneManager.LoadScene(goToScene);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        
+	}
+
+    void OnMouseDown ()
+    {
+        if (currentScene != 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- Home Street Home/Assets/Scripts/gameMaster.cs
+++ Home Street Home/Assets/Scripts/gameMaster.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameMaster : MonoBehaviour {
     private bool showDiary;
     public GameObject Diary;
+    public Text cashText; 
+
+    private float cash = 12.0f; 
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +17,8 @@ public class gameMaster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             showDiary = !showDiary;
         }
@@ -26,5 +31,6 @@ public class gameMaster : MonoBehaviour {
         {
             Diary.SetActive(false);
         }
-	}
+        cashText.text = "£" + cash;
+    }
 }
