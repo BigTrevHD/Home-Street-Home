f228863122a3bd527627db67240c97c937ad0647
BigTrevHD <34038408+BigTrevHD@users.noreply.github.com>
2019-03-19 18:44:11 +0000

    Consuming food
    -Eating & drinking food will increase respective stats

---
+++ Home Street Home/Assets/Scripts/Consume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consume : MonoBehaviour {
+    public float addHunger = 0;
+    public float addThirst = 0;
+    public float addWarmth = 0;
+
+    public playerStats stats;
+
+    // Use this for initialization
+    void Start () {
+        stats = GameObject.FindGameObjectWithTag("gm").GetComponent<playerStats>();
+        
+
+    }
+	
+	// Update is called once per frame
+	public void UseItem ()
+    {
+        Destroy(gameObject);
+        stats.Hunger += addHunger;
+        stats.Thirst += addThirst;
+        stats.Warmth += addWarmth;
+
+        
+    }
+}
