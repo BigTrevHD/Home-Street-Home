164e7722b97579b77b0d7d9eccfe62f766c3bd1f
TOPHATs64GAMEs <34027835+TOPHATs64GAMEs@users.noreply.github.com>
2019-03-21 10:14:53 +0000

    Added the start of the rummage mechanics

---
+++ Home Street Home/Assets/Scripts/mechanics/Rummaging.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rummaging : MonoBehaviour
+{
+
+    private float randomValue;
+    private GameObject bin;
+
+    public bool collided;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (collided == true)
+        {
+            collided = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("r"))
+        {
+            randomValue = Random.Range(0, 100);
+            if (collided == true)
+            {
+
+
+                if (randomValue <= 33)
+                {
+                    Debug.Log("Found Half Bacon Sandwich");
+                }
+                else if (randomValue >= 66)
+                {
+                    Debug.Log("Found Coffee Dregs");
+                }
+                else
+                {
+                    Debug.Log("Found Nothing");
+                }
+            }
+        }
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            if (Input.GetKeyDown("r"))
+            {
+                collided = true;
+                Debug.Log("Collided With Bin");
+            }
+        }
+    }
+}
+
